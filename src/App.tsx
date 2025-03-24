428d343362eb00d394d80b8c17b497d803850ba6
jessicapinheiro-s <jessicasilva.js1314@gmail.com>
2025-03-24 19:09:52 -0300

    Adicionando um caminho no App para acessar a página de favoritos.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PrincipaisCoinsPage from './pages/Pincipais-coins'
 import AllCoins from './pages/All-Coins'
 import About from './pages/About'
 import CoinInfoPage from './pages/Coin-Info-Page'
+import FavoriteCoins from './pages/Favorite-Coins'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<PrincipaisCoinsPage />} />
         <Route path="/About" element={< About/>} />
         <Route path="/All-Coins" element={<AllCoins />} />
+        <Route path="/Favorite-Coins" element={<FavoriteCoins />} />
         <Route path="/Coin-Info-Page" element={<CoinInfoPage />} />
       </Routes>
     </>
